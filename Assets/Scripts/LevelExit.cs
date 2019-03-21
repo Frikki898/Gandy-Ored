a0d8be78b74a56efece8ba97ab3cdcf8124fec37
Guðjón Guðjónsson <gudjon15@ru.is>
2019-03-21 02:15:17 +0000

    added on exit prefab

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -4,16 +4,79 @@ using UnityEngine;
 
 public class LevelExit : MonoBehaviour
 {
-	public int 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public int selectLevel;
+	public bool canExit = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public GameObject cam1;
+
+	private bool gnomeExit = false;
+	private bool ogreExit = false;
+
+	private GameObject ogre = null;
+	private GameObject gnome = null;
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (canExit)
+		{
+			if (collision.gameObject.name == "gnome")
+			{
+				Debug.Log("gnome can now exit the level");
+				gnome = collision.gameObject;
+				gnomeExit = true;
+			}
+
+			if (collision.gameObject.name == "ogre")
+			{
+				Debug.Log("ogre can now exit the level");
+				ogre = collision.gameObject;
+				ogreExit = true;
+			}
+
+			if (gnomeExit && ogreExit)
+			{
+				Debug.Log("We can switch scene");
+				setNextLevel();
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject == gnome)
+		{
+			gnome = null;
+			gnomeExit = false;
+		}
+		if (collision.gameObject == ogre)
+		{
+			ogre = null;
+			ogreExit = false;
+		}
+	}
+
+	void setNextLevel()
+	{
+		if (selectLevel == 1)
+		{
+			Debug.Log("should relocate");
+			gnome.transform.position = new Vector3(-16.85f, -14.73015f, -40.3f);
+			ogre.transform.position = new Vector3(-25.21f, -14.71885f, -38.5f);
+
+			cam1.transform.position = new Vector3(-5.989776f, -1.957116f, -45);
+		}
+		//switch (selectLevel)
+		//{
+		//	case 1:
+		//		Debug.Log("should relocate");
+		//		gnome.transform.position = new Vector3(-16.85f, -14.73015f, -40.3f);
+		//		ogre.transform.position = new Vector3(-25.21f, -14.71885f, -38.5f);
+		//		break;
+		//	case 2:
+		//		break;
+		//	default:
+
+		//		break;
+		//}
+	}
 }
