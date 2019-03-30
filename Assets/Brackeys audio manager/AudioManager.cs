3707b1a7eb0ab07e6e601a4297f199fbe653ff07
Frikki898 <frikki898@gmail.com>
2019-03-30 20:24:28 +0000

    added some levels and added audio manager that we can use if we want sounds

---
+++ Assets/Brackeys audio manager/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound currSound = Array.Find(sounds, sound => sound.name == name);
+        if(currSound == null)
+        {
+            return;
+        }
+        currSound.source.Play();
+    }
+}
